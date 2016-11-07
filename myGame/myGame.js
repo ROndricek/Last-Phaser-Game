a77d2f0c9a2c4f2a16343df40cfba78dd695acf2
Ryan <ryondricek.sh@mysummitps.org>
2016-11-07 20:14:31 +0000

    update

--- myGame/myGame.js
+++ myGame/myGame.js
@@ -1,7 +1,7 @@
 /*global Phaser*/
 
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
+var game = new Phaser.Game(1900, 600, Phaser.AUTO, '');
 var game_state = {}
 
 var mouseX;
@@ -17,23 +17,23 @@ game_state.main.prototype = {
 
 
     preload: function() {
-        game.load.image('sky', 'assets/sky.png');
         game.load.image('ground' , 'assets/platform.png');
         game.load.image('star' , 'assets/star.png');
         game.load.spritesheet('dude' , 'assets/dude.png', 32, 48);
+        game.load.image('haunt' , 'assets/download.png');
     },
 
 
     create: function() {
         game.add.sprite(0, 0, 'star');
-        game.add.sprite(0, 0, 'sky');
+        game.add.sprite(0, 0, 'haunt');
         this.platforms = game.add.group();
         this.platforms.enableBody = true;
         
         var ground = this.platforms.create(0, game.world.height - 64, 'ground');
-        ground.scale.setTo(2, 2);
+        ground.scale.setTo(10, 2);
         ground.body.immovable = true
-        var ledge = this.platforms.create(59, 60, 'ground');
+        var ledge = this.platforms.create(37, 193, 'ground');
         ledge.width = 100;
         ledge.body.immovable = true;
         ledge = this.platforms.create(50, 400, 'ground');
@@ -50,17 +50,26 @@ game_state.main.prototype = {
         ledge = this.platforms.create(626, 484, 'ground');
         ledge.body.immovable = true;
         ledge.width = 100;
+        ledge = this.platforms.create(979, 207, 'ground');
+        ledge.body.immovable = true;
+        ledge = this.platforms.create(776, 340, 'ground');
+        ledge.body.immovable = true;
+        ledge.width = 100;
+        ledge = this.platforms.create(1104, 340, 'ground');
+        ledge.body.immovable = true;
+        ledge.width = 100;
+        ledge = this.platforms.create(965, 457, 'ground');
+        ledge.body.immovable = true;
+        ledge.width = 100;
        
        
        
-       
-       
-        game.physics.startSystem(Phaser.Physics.ARCADE)
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         this.player = game.add.sprite(32, game.world.height - 150, 'dude');
         game.physics.arcade.enable(this.player);
         this.player.body.bounce.y = 0.1;
         this.player.body.gravity.y = 1000;
-        this.player.body.collideWorldBounds = true;
+        //this.player.body.collideWorldBounds = true;
         this.player.animations.add( 'left', [0, 1, 2, 3,], 10, true);
         this.player.animations.add( 'right', [5, 6, 7, 8,], 10, true);
         this.player.body.checkCollision.up = false;
@@ -72,12 +81,13 @@ game_state.main.prototype = {
             var star = this.stars.create(i * 70, 0, "star");
             star.body.gravity.y = 300;
             star.body.bounce.y = 0.7 + Math.random() * 0.2;
-         this.scoreText = game.add.text(16, 16, 'score: 0', {
+        }
+             this.scoreText = game.add.text(16, 16, 'score: 0', {
              fontSize: '32px',
              fill: '#000'
          });
-            
-        }
+         
+         this.score = 0;
         
         
 
@@ -119,6 +129,8 @@ game_state.main.prototype = {
     
     collectStar: function(player, star) {
         star.kill();
+        this.score += 1;
+        this.scoreText.text = "Score: " + this.score;
     }
 
 }
